a8b761f8316148a349bae944f51fa75f217fea5f
Xander Pradilla Porras <xderporras1013@gmail.com>
2026-08-14 16:39:11 +0800

    Fixed CSLAB2 folder structure on Windows

---
+++ CSLAB2/Page2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $pageTitle = "Xander's Portfolio"; ?></title>
+</head>
+<body>
+    <center>
+        <h1><?php echo $header = "About Me Page"; ?></h1>
+
+    <img src = "pics/755033411_1029110336426190_8962152450340070681_n.jpg" alt = "My Dogs" width="500" height="700">
+
+    <p> <?php echo "My Name is Xander Porras. I am a student of USLS. Currently on my third year of studying for my Bachelor's Degree in Computer Science.<br>
+    This is my portfolio website as well a project. I am a dog lover as you can see in the picture. I love to play with my dogs as well as playing video games. <br>
+    I love eating fried chicken and rice. My favorite drink is Iced Coffee."; ?></p>
+
+    <a href="Page1.php">Home Page</a>
+    <br>
+    <a href="Page3.php">Contact Me Page</a>
+    </center>
+
+</body>
+</html>
